f5a02f4d8833264aaaeed189783c95144b13cb00
ZacharyOuellet <113061957+ZacharyOuellet@users.noreply.github.com>
2024-02-03 18:30:40 -0500

    setup?

--- server-rust/src/main.rs
+++ server-rust/src/main.rs
@@ -1,8 +1,13 @@
 use axum::{routing::get, Router};
 
+pub mod pathfinding;
+use pathfinding::a_star;
+
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(|| async { "Hello, Rust!" }));
+
+    a_star::test();
 
     println!("Running on http://localhost:3000");
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
