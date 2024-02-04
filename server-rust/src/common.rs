e83064c7f596d6e743470f8c8901582983f2d489
Kujaku223 <Daniuriza@outlook.com>
2024-02-04 01:09:24 -0500

    pushing before I fuck up

--- server-rust/src/common.rs
+++ server-rust/src/common.rs
@@ -2,10 +2,12 @@ use std::fmt::Debug;
 
 use serde::Serialize;
 use serde::ser::SerializeStruct;
+
 pub struct Node {
     pub x: f32,
-    pub y: f32
+    pub y: f32,
 }
+
 impl Serialize for Node {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
@@ -24,14 +26,48 @@ impl Debug for Node {
     }
 }
 
+pub struct Graph {
+    pub nodes: Vec<Node>,
+    pub edges: Vec<Edge>,
+}
 
+impl Serialize for Graph {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut state = serializer.serialize_struct("Graph", 2)?;
+        state.serialize_field("nodes", &self.nodes)?;
+        state.serialize_field("edges", &self.edges)?;
+        state.end()
+    }
+}
 
-pub struct Edge{
+impl Debug for Graph {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Graph {{ nodes: {:?}, edges: {:?} }}", self.nodes, self.edges)
+    }
+}
+
+pub struct Edge {
     pub from_index: usize,
     pub to_index: usize,
 }
 
-pub struct Graph{
-    pub nodes: Vec<Node>,
-    pub edges: Vec<Edge>,
+impl Serialize for Edge {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut state = serializer.serialize_struct("Edge", 2)?;
+        state.serialize_field("from_index", &self.from_index)?;
+        state.serialize_field("to_index", &self.to_index)?;
+        state.end()
+    }
+}
+
+impl Debug for Edge {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Edge {{ from_index: {}, to_index: {} }}", self.from_index, self.to_index)
+    }
 }
--- server-rust/src/grid_generator/gridgeneration.rs
+++ server-rust/src/grid_generator/gridgeneration.rs
@@ -1,31 +1,126 @@
+
+use crate::common::{Edge, Node};
 use rand::prelude::*;
 use serde_json;
-use crate::common::{Node, Edge, Graph};
 
 pub fn generate_grid() {
-        let mut nodes: Vec<(f64, f64)> = Vec::new();
-        let mut lines: Vec<usize> = Vec::new();
+    let mut nodes: Vec<Node> = Vec::new();
+    let mut edges: Vec<Edge> = Vec::new();
 
-        nodes.push((thread_rng().gen_range(0.0..1.0), thread_rng().gen_range(0.0..1.0)));
-        for i in 0..98 {
-                nodes.push((thread_rng().gen_range(0.0..1.0), thread_rng().gen_range(0.0..1.0)));
-                for j in 0..i {
-                        
+    nodes.push(create_random_node());
+
+    for i in 0..98 {
+        nodes.push(create_random_node());
+        let mut available_nodes: Vec<usize> = Vec::new();
+
+        for (index, node) in nodes.iter().enumerate() {
+                if check_if_edge_is_valid(&edges, &nodes) {
+                        available_nodes.push(index);
                 }
-                lines.push(i);
-                //take the last node na
+        }
+        let mut k = 0;
+        while k <= 5 {
+            // Do something in the loop
+            if available_nodes.last().is_none()
+            {break;}
+            
+                k += 1;}    
+        }
+        
+    println!("{:?}", nodes);
+    println!("{:?}", edges);
+
+    // Convert the arrays to JSON
+    let nodes_json = serde_json::to_string(&nodes)
+        .unwrap_or_else(|err| panic!("Failed to convert nodes to JSON: {}", err));
+    let edges_json = serde_json::to_string(&edges)
+        .unwrap_or_else(|err| panic!("Failed to convert edges to JSON: {}", err));
+
+    // Save the JSON to files
+    std::fs::write("src/data_nodes.json", &nodes_json)
+        .unwrap_or_else(|err| panic!("Failed to write nodes JSON to file: {}", err));
+    std::fs::write("src/data_edges.json", &edges_json)
+        .unwrap_or_else(|err| panic!("Failed to write edges JSON to file: {}", err));
+
+    println!("done!");
+}
+
+pub fn check_if_edge_is_valid(edges: &Vec<Edge>, nodes: &Vec<Node>) -> bool {
+    // Check if the edge is valid
+    // Return true if valid, false otherwise
+    let mut is_valid = false;
+    let mut edge: Edge = Edge {
+        from_index: nodes.len(),
+        to_index: nodes.len(),
+    };
+
+    for i in 0..nodes.len() {
+        if nodes.get(nodes.len()-1).unwrap().x < nodes.get(i).unwrap().x {
+            edge = Edge {
+                from_index: nodes.len() - 1,
+                to_index: i,
+            };
+        } else {
+            edge = Edge {
+                from_index: i,
+                to_index: nodes.len() - 1,
+            };
         }
 
-        println!("{:?}", nodes);
-        println!("{:?}", lines);
+        for possibleCollision in edges.iter() {
+            let point11 = nodes.get(edge.from_index).unwrap();
+            let point12 = nodes.get(edge.to_index).unwrap();
+            let slope1 = (point12.y - point11.y) / (point12.x - point11.x);
 
-        // Convert the arrays to JSON
-        let nodes_json = serde_json::to_string(&nodes).unwrap_or_else(|err| panic!("Failed to convert nodes to JSON: {}", err));
-        let lines_json = serde_json::to_string(&lines).unwrap_or_else(|err| panic!("Failed to convert lines to JSON: {}", err));
+            let point21 = nodes.get(possibleCollision.from_index).unwrap();
+            let point22 = nodes.get(possibleCollision.to_index).unwrap();
+            let slope2 = (point22.y - point21.y) / (point22.x - point21.x);
+            if might_touch(&edge, &possibleCollision, &nodes) {
+                if ((slope1 > 0.0 && slope2 > 0.0) || (slope1 < 0.0 && slope2 < 0.0))
+                    && slope1 != slope2
+                {
+                        is_valid = true;   
+                } else {
+                        is_valid = false;
+                }
+            }
+        }
+    }
+    is_valid
+}
 
-        // Save the JSON to files
-        std::fs::write("src/data_nodes.json", &nodes_json).unwrap_or_else(|err| panic!("Failed to write nodes JSON to file: {}", err));
-        std::fs::write("src/data_lines.json", &lines_json).unwrap_or_else(|err| panic!("Failed to write lines JSON to file: {}", err));
+pub fn might_touch(edge1: &Edge, edge2: &Edge, nodes: &Vec<Node>) -> bool {
+    let mut touches = false;
 
-        println!("done!");
+    let point11 = nodes.get(edge1.from_index).unwrap();
+    let point12 = nodes.get(edge1.to_index).unwrap();
+
+    let point21 = nodes.get(edge2.from_index).unwrap();
+    let point22 = nodes.get(edge2.to_index).unwrap();
+
+    let max_x1 = point11.x.max(point12.x);
+    let min_x1 = point11.x.min(point12.x);
+
+    let max_x2 = point21.x.max(point22.x);
+    let min_x2 = point21.x.min(point22.x);
+
+    if max_x1 > min_x2 || max_x2 > min_x1 {
+        let max_y1 = point11.y.max(point12.y);
+        let min_y1 = point11.y.min(point12.y);
+
+        let max_y2 = point21.y.max(point22.y);
+        let min_y2 = point21.y.min(point22.y);
+
+        if max_y1 > min_y2 || max_y2 > min_y1 {
+            touches = true;
+        }
+    }
+    touches
+}
+
+pub fn create_random_node() -> Node {
+        Node {
+                x: thread_rng().gen_range(0.0..1.0),
+                y: thread_rng().gen_range(0.0..1.0),
+        }
 }
