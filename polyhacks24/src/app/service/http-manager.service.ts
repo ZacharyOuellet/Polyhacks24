bc8b0b56b7836e179ccb54757a62f31f6e50a96f
Kujaku223 <Daniuriza@outlook.com>
2024-02-04 09:23:35 -0500

    Everything is empty

--- polyhacks24/src/app/service/http-manager.service.ts
+++ polyhacks24/src/app/service/http-manager.service.ts
@@ -6,9 +6,18 @@ import { HttpClient } from '@angular/common/http';
 export class HttpManagerService {
   constructor(private http: HttpClient) {}
 
-  getgrap() {
+ async getgrap() {
+    return await this.http.get('http://localhost:3000/graph');
 }
 
-getSolution(graph: any, driverStart: any, driverEnd: any, passengerStart: any, passengerEnd: any) {
+async getSolution(graph: any, driverStart: any, driverEnd: any, passengerStart: any, passengerEnd: any) {
+  let requestBody = {
+    graph: graph,
+    driverStart: driverStart,
+    driverEnd: driverEnd,
+    passengerStart: passengerStart,
+    passengerEnd: passengerEnd
+  }
+  return await this.http.post('http://localhost:3000/solution', {requestBody});
 }
 }
