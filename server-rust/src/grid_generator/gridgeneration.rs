d162e216ec11f23f6fba87e4177f3b613760b4b1
Kujaku223 <Daniuriza@outlook.com>
2024-02-04 02:31:41 -0500

    re

--- server-rust/src/grid_generator/gridgeneration.rs
+++ server-rust/src/grid_generator/gridgeneration.rs
@@ -1,4 +1,3 @@
-
 use crate::common::{Edge, Node};
 use rand::prelude::*;
 use serde_json;
@@ -22,18 +21,26 @@ pub fn generate_grid() {
         while k <= 5 {
             // Do something in the loop
             if available_nodes.last().is_none() {
+                print!("RAWR");
                 break;
             }
             let mut rng = thread_rng();
             edges.shuffle(&mut rng);
             let i2 = available_nodes.pop().unwrap();
-            edges.push(Edge {
-                from_index: i,
-                to_index: i2,
-            });
+            if nodes.get(i).unwrap().x < nodes.get(i2).unwrap().x {
+                edges.push(Edge {
+                    from_index: i,
+                    to_index: i2,
+                });
+            } else {
+                edges.push(Edge {
+                    from_index: i2,
+                    to_index: i,
+                });
+            }
             k += 1;
         }
-    }   
+    }
     println!("{:?}", nodes);
     println!("{:?}", edges);
 
@@ -55,14 +62,14 @@ pub fn generate_grid() {
 pub fn check_if_edge_is_valid(edges: &Vec<Edge>, nodes: &Vec<Node>) -> bool {
     // Check if the edge is valid
     // Return true if valid, false otherwise
-    let mut is_valid = false;
+    let mut is_valid = true;
     let mut edge: Edge = Edge {
         from_index: nodes.len(),
         to_index: nodes.len(),
     };
 
-    for i in 0..nodes.len() {
-        if nodes.get(nodes.len()-1).unwrap().x < nodes.get(i).unwrap().x {
+    for i in 0..nodes.len()-1 {
+        if nodes.get(nodes.len() - 1).unwrap().x < nodes.get(i).unwrap().x {
             edge = Edge {
                 from_index: nodes.len() - 1,
                 to_index: i,
@@ -82,13 +89,13 @@ pub fn check_if_edge_is_valid(edges: &Vec<Edge>, nodes: &Vec<Node>) -> bool {
             let point21 = nodes.get(possibleCollision.from_index).unwrap();
             let point22 = nodes.get(possibleCollision.to_index).unwrap();
             let slope2 = (point22.y - point21.y) / (point22.x - point21.x);
+
             if might_touch(&edge, &possibleCollision, &nodes) {
-                if ((slope1 > 0.0 && slope2 > 0.0) || (slope1 < 0.0 && slope2 < 0.0))
-                    && slope1 != slope2
+                if (slope1 > 0.0 && slope2 > 0.0) || (slope1 < 0.0 && slope2 < 0.0)
                 {
-                        is_valid = true;   
+                    is_valid = true;
                 } else {
-                        is_valid = false;
+                    is_valid = false;
                 }
             }
         }
@@ -111,14 +118,14 @@ pub fn might_touch(edge1: &Edge, edge2: &Edge, nodes: &Vec<Node>) -> bool {
     let max_x2 = point21.x.max(point22.x);
     let min_x2 = point21.x.min(point22.x);
 
-    if max_x1 > min_x2 || max_x2 > min_x1 {
+    if max_x1 < min_x2 || max_x2 < min_x1 {
         let max_y1 = point11.y.max(point12.y);
         let min_y1 = point11.y.min(point12.y);
 
         let max_y2 = point21.y.max(point22.y);
         let min_y2 = point21.y.min(point22.y);
 
-        if max_y1 > min_y2 || max_y2 > min_y1 {
+        if max_y1 < min_y2 || max_y2 < min_y1 {
             touches = true;
         }
     }
@@ -126,8 +133,8 @@ pub fn might_touch(edge1: &Edge, edge2: &Edge, nodes: &Vec<Node>) -> bool {
 }
 
 pub fn create_random_node() -> Node {
-        Node {
-                x: thread_rng().gen_range(0.0..1.0),
-                y: thread_rng().gen_range(0.0..1.0),
-        }
+    Node {
+        x: thread_rng().gen_range(0.0..1.0),
+        y: thread_rng().gen_range(0.0..1.0),
+    }
 }
